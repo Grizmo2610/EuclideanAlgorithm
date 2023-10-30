478a3e39a0eeb3684ce8f9cec3ae8f4db53d3a00
Grizmo2610 <hoangtuantu893@gmail.com>
2023-10-30 23:10:55 +0700

    commit

--- app.js
+++ app.js
@@ -8,57 +8,67 @@ class Viewer {
     }
 
 
-    createItem(a,b) {
+    createItem(a, b) {
         this.setPattern(20);
-        if(this.item) {
+        if (this.item) {
             this.item.dispose();
             this.item = null;
         }
-        this.item = new Item(this, 0,0,a,b);        
+        this.item = new Item(this, 0, 0, a, b);
     }
 
     setPattern(u) {
-        if(u == this.u) return;
+        if (u == this.u) return;
         this.u = u;
-        if(this.pattern) this.pattern.remove(); 
+        if (this.pattern) this.pattern.remove();
         const d = 3;
-        this.pattern = this.draw.pattern(u, u, function(add) {
-            add.rect(u,u).fill('#47F');
-            add.circle(u-2*d).fill('#36e').stroke("#136").move(d,d);
+        this.pattern = this.draw.pattern(u, u, function (add) {
+            add.rect(u, u).fill('#47F');
+            add.circle(u - 2 * d).fill('#36e').stroke("#136").move(d, d);
         });
     }
 
-    createRect(x,y,w,h) {
+    createRect(x, y, w, h) {
         x = this.u * x + 0.5;
         y = this.u * y + 0.5;
         w = this.u * w;
         h = this.u * h;
-        return this.draw.rect(w,h).fill(this.pattern).stroke("black").move(x,y);        
+        return this.draw.rect(w, h).fill(this.pattern).stroke("black").move(x, y);
     }
 
     dispose() {
         this.pattern.delete();
         this.pattern = null;
         this.u = 0;
-        if(this.item) {
+        if (this.item) {
             this.item.dispose();
             this.item = null;
         }
     }
 
     getStatusMessage() {
-        if(!this.item) return "";
-        const {a,b} = this.item;
-        const [aa,bb] = this.item.getCurrentSize();
+        if (!this.item) return "";
+        const { a, b } = this.item;
+        const [aa, bb] = this.item.getCurrentSize();
         let msg = "GCD(" + a + ", " + b + ") = ";
-        if(a==aa && b==bb) msg += "?";
-        else if(aa==bb) msg += aa;
+        if (a == aa && b == bb) msg += "?";
+        else if (aa == bb) msg += aa;
         else msg += "GCD(" + aa + ", " + bb + ")";
-        return msg;        
+        return msg;
     }
 }
 
 class Item {
+    euclideanGCD(a, b) {
+        while (b !== 0) {
+            const temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+
     constructor(rectMaker, x0, y0, a, b) {
         this.a = a;
         this.b = b;
@@ -67,39 +77,45 @@ class Item {
         this.squares = [];
         this.child = null;
         this.state = 0;
-        this.rect = rectMaker.createRect(x0,y0,a,b);
-        if(a>=b) {
-            let r = a%b;
-            for(let i=0; r+b*(i+1)<=a;i++) 
-                this.squares.push(rectMaker.createRect(x0+r+b*i,y0,b,b));
-            if(r!=0)
-                this.child = new Item(rectMaker,x0,y0,r,b);
+        this.rect = rectMaker.createRect(x0, y0, a, b);
+        this.gcd = this.euclideanGCD(a, b);
+        if (a >= b) {
+            let r = a % b;
+            for (let i = 0; r + b * (i + 1) <= a; i++)
+                this.squares.push(rectMaker.createRect(x0 + r + b * i, y0, b, b));
+            if (r != 0)
+                this.child = new Item(rectMaker, x0, y0, r, b);
         } else {
-            let r = b%a;
-            for(let i=0; r+a*(i+1)<=b;i++) 
-                this.squares.push(rectMaker.createRect(x0,y0+r+a*i,a,a));
-            if(r!=0) this.child = new Item(rectMaker,x0,y0,a,r);
+            let r = b % a;
+            for (let i = 0; r + a * (i + 1) <= b; i++)
+                this.squares.push(rectMaker.createRect(x0, y0 + r + a * i, a, a));
+            if (r != 0) this.child = new Item(rectMaker, x0, y0, a, r);
         }
-        this.squares.forEach(square=>square.hide());
+        this.squares.forEach(square => square.hide());
         let c = this.child;
-        while(c) {c.rect.hide();c=c.child;}
+        while (c) { c.rect.hide(); c = c.child; }
 
     }
 
     stepForward() {
-        if(this.state == 0) {
+        if (this.state === 0) {
             this.rect.hide();
-            this.squares.forEach(square=>square.show());
-            if(this.child) this.child.rect.show();
+            this.squares.forEach(square => square.show());
+            if (this.child) this.child.rect.show();
             this.state = 1;
-        } else if (this.state<=this.squares.length) {
+        }else if(this.curA == this.curB){
+            this.rect.hide();
+            this.squares.forEach(square => square.show());
+            if (this.child) this.child.rect.show();
+            this.state = 1;
+        }else if (this.state <= this.squares.length) {
             let j = this.squares.length - this.state;
-            let d = 100 + 30 * (j-1);
+            let d = 100 + 30 * (j - 1);
             let horizontal = this.a >= this.b;
-            let dx = horizontal ? d : 0, dy = horizontal ? 0 : d;     
-            let square = this.squares[j];       
-            square.animate().translate(dx,dy).animate().opacity(0).after(()=>square.hide());
-            if(horizontal) this.curA = this.a - this.state * this.b;
+            let dx = horizontal ? d : 0, dy = horizontal ? 0 : d;
+            let square = this.squares[j];
+            square.animate().translate(dx, dy).animate().opacity(0).after(() => square.hide());
+            if (horizontal) this.curA = this.a - this.state * this.b;
             else this.curB = this.b - this.state * this.a;
             this.state += 1;
         } else {
@@ -107,16 +123,17 @@ class Item {
         }
     }
 
+
     dispose() {
-        if(this.child) this.child.dispose();
+        if (this.child) this.child.dispose();
         this.rect.remove();
-        this.squares.forEach(square=>square.remove());
+        this.squares.forEach(square => square.remove());
     }
 
     getCurrentSize() {
-        if(this.state <= this.squares.length) return [this.curA, this.curB];
-        else if(this.child) return this.child.getCurrentSize();
-        else return [0,0];
+        if (this.state <= this.squares.length) return [this.curA, this.curB];
+        else if (this.child) return this.child.getCurrentSize();
+        else return [0, 0];
     }
 }
 
@@ -131,7 +148,7 @@ function initialize() {
 
 function stepForward() {
     let item = viewer?.item;
-    if(item) {
+    if (item) {
         item.stepForward();
         updateStatusMessage();
     }
@@ -161,7 +178,7 @@ function updateStatusMessage() {
 function foo() {
     let a = document.getElementById("A").valueAsNumber;
     let b = document.getElementById("B").valueAsNumber;
-    viewer.createItem(a,b);   
-    updateStatusMessage(); 
+    viewer.createItem(a, b);
+    updateStatusMessage();
 }
 
